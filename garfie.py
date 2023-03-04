8ba4a12581fa8c0e6b9b49bbc9a1005eec56d948
j-p-1 <jp01220414@gmail.com>
2023-03-04 10:47:17 -0500

    Create initial reimplementation

--- garfie.py
+++ garfie.py
@@ -1,16 +1,17 @@
-from PIL import Image
+import pygame, sys # import the needed modules
 
-#...
+pygame.init() # Init Pygame
 
-img1 = Image.open('GarfImages/garf1.jpg')
-img2 = Image.open('GarfImages/garf2.jpg')
-img3 = Image.open('GarfImages/garf3.jpg')
-img4 = Image.open('GarfImages/garf4.jpg')
+display = pygame.display.set_mode((800, 800)) # Create a 800 pixel by 800 pixel display window
+
+pygame.display.set_caption("Buff Garfie") # Set window title
+
+# Mainloop:
 
 zoop = True
 
-while (zoop==True):
-    img1.show()
-    img2.show()
-    img3.show()
-    img4.show()
+while zoop:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit() # Stop Pygame
+			sys.exit() # Exit the window as after calling the above, the window with the content just hangs.
